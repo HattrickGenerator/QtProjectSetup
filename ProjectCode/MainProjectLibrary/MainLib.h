ce3d1aa5c917270b4df608054ea76d8e28d0267a
Michael Gilch <michaelgilch93@gmail.com>
2020-09-17 15:25:39 +0200

    First Example Project of an embedded 3D rendering application

--- ProjectCode/MainProjectLibrary/MainLib.h
+++ ProjectCode/MainProjectLibrary/MainLib.h
@@ -1,18 +1,48 @@
 #include <QMainWindow>
-#include <QTextEdit>
+#include <QVBoxLayout>
+//#include "ButtonWidget.h"
+//#include "LightWidget.h"
+
+#include <QPushButton>
+#include "Widget3DContainer.h"
+
+#include <QQuickView>
+#include <Qt3DExtras/Qt3DWindow>
+#include <QOrbitCameraController>
+
 
 class CustomMainWindow : public QMainWindow
 {
+    Q_OBJECT
+
 public:
 
  CustomMainWindow() : QMainWindow()
     {
-     this->setCentralWidget(&mainWidget);
-     textEdit.setParent(&mainWidget);
+    mainWidget = new QWidget();
+
+     layout = new QVBoxLayout();
+
+     containerWidget = new Widget3DContainer(this);
+    layout->addWidget(containerWidget);
+
+     mainWidget->setLayout(layout);
+     setCentralWidget(mainWidget);
+
     }
 
-private:
-    QWidget mainWidget;
-    QTextEdit textEdit;
+public:
+    virtual void closeEvent ( QCloseEvent * event )
+ {
+     delete containerWidget;
+     event->accept();
+ }
 
+signals:
+ void mainWindowClosed();
+
+private:
+    Widget3DContainer* containerWidget;
+    QWidget *mainWidget;
+    QVBoxLayout *layout;
 };
---
+++ ProjectCode/MainProjectLibrary/Widget3DContainer.h
@@ -0,0 +1,36 @@
+#include <QWidget>
+#include <QHBoxLayout>
+#include <Qt3DExtras/Qt3DWindow>
+#include <Qt3DExtras/QOrbitCameraController>
+
+#include "OrbitTransformController.h"
+#include "createScene.h"
+
+#include <Qt3DCore/QEntity>
+#include <Qt3DRender/QCamera>
+#include <Qt3DRender/QCameraLens>
+#include <Qt3DCore/QTransform>
+#include <Qt3DCore/QAspectEngine>
+#include <Qt3DInput/QInputAspect>
+#include <Qt3DRender/QRenderAspect>
+#include <Qt3DExtras/QForwardRenderer>
+#include <Qt3DExtras/QPhongMaterial>
+#include <Qt3DExtras/QCylinderMesh>
+#include <Qt3DExtras/QSphereMesh>
+#include <Qt3DExtras/QTorusMesh>
+#include <QPropertyAnimation>
+#include "OrbitTransformController.h"
+
+
+class Widget3DContainer : public QWidget
+{
+    QOBJECT_H
+
+public:
+    Widget3DContainer(QWidget *parent = nullptr);
+    virtual ~Widget3DContainer();
+
+private:
+    Qt3DCore::QEntity *createScene();
+   QWidget *container;
+};
